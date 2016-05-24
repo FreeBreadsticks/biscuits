0484a25fbbeb299201470e00bd84033cfb745dc7
Erika <erika.hughes@dbspublishing.com>
2016-05-24 00:18:16 +0000

    add new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biscuits::Application.config.secret_key_base = '886db971fcc79a4711df05184dbcbd06c5997e201a9fd18a5a8690a2f823f7ebdf3eb03043a26642338dfbc72e74fa350d8116ca432baa3590d5b10bbde37bfa'
